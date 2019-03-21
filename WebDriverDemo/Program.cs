c0a8ff6ef8c48b2c0e7455698fe082248300aa3b
Marisol Colon <mcolon@foundationmedicine.com>
2019-03-20 18:43:35 -0600

    Add project files.

---
+++ WebDriverDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox; 
+
+namespace WebDriverDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "http://www.google.com";
+            
+        }
+    }
+}
